1db48e88fdc37c74cb1c543fd25abea85565567f
Devlin <devlincodes@gmail.com>
2024-04-23 15:43:06 -0700

    change index render

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const { MongoClient } = require("mongodb");
-
 require("dotenv").config();
+
 const uri = process.env.MONGODB_CONNECTION;
 const client = new MongoClient(uri);
-
-router.get("/", async (req, res, next) => {
-  const posts = await getPosts().catch(console.dir);
-
-  res.render("layout", {
-    title: "Members Only",
-    content: "index",
-    stylesheet: "/stylesheets/style.css",
-    message_list: posts,
-  });
-});
+const connectToDatabase = require("../utils/functions");
 
 async function getPosts() {
   try {
-    const database = client.db("dev");
+    const database = client.db(process.env.DATABASE);
     const posts = database.collection("posts");
     const data = await posts.find().toArray();
     return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+router.get("/", async (req, res, next) => {
+  try {
+    await connectToDatabase(client);
+    const posts = await getPosts().catch(console.dir);
+
+    res.render("layout", {
+      title: "Members Only",
+      content: "index",
+      stylesheet: "/stylesheets/style.css",
+      post_list: posts,
+    });
+  } catch (error) {
+    console.error(error);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
-}
+});
 
 module.exports = router;
