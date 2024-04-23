f93d22d7ab748ddffe11713ad2fc9497841fd348
Devlin <devlincodes@gmail.com>
2024-04-23 15:45:11 -0700

    enable user sign-up with password hashing & basic input validations

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/sign-up");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -19,20 +20,21 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/sign-up", userRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((error, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = error.message;
+  res.locals.error = req.app.get("env") === "development" ? error : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(error.status || 500);
   res.render("error");
 });
 
---
+++ routes/sign-up.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+require("dotenv").config();
+
+const uri = process.env.MONGODB_CONNECTION;
+const client = new MongoClient(uri);
+const connectToDatabase = require("../utils/functions");
+
+async function getUser(email) {
+  try {
+    const database = client.db(process.env.DATABASE);
+    const posts = database.collection("users");
+    const data = await posts.findOne({ email });
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+router.get("/", async (req, res, next) => {
+  try {
+    await connectToDatabase(client);
+
+    res.render("layout", {
+      title: "Members Only",
+      content: "sign_up",
+      stylesheet: "/stylesheets/style.css",
+      errors: null,
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+});
+
+router.post(
+  "/",
+  async (req, res, next) => {
+    // Handle the request
+
+    try {
+      await connectToDatabase(client);
+    } catch (error) {
+      return next(error);
+    } finally {
+      return next();
+    }
+  },
+
+  body("email")
+    .isEmail()
+    .withMessage("valid email is required")
+    .custom(async (value) => {
+      const user = await getUser(value);
+
+      if (user) {
+        throw new Error("email already in use");
+      }
+    }),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("password is not long enough"),
+  body("passwordConfirmation").custom((value, { req }) => {
+    return value === req.body.password;
+  }),
+
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      console.log({ errors });
+      console.log(errors.errors, errors.formatter);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).render("layout", {
+          title: "Members Only",
+          content: "sign_up",
+          message: "Something went wrong",
+          errors: errors.errors,
+          stylesheet: "/stylesheets/style.css",
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+      const database = client.db(process.env.DATABASE);
+      const users = database.collection("users");
+
+      await users.insertOne({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+
+      res.redirect("/");
+    } catch (error) {
+      return next(error);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+);
+
+module.exports = router;
